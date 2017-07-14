d596a53635ce67989b593b6a9792f4d8e0d1baa0
Johnny Watson <johnny@streetchange.org.uk>
2017-07-14 18:18:42 +0100

    Add inventory component and build out header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,17 @@ class Header extends React.Component {
 
   render() {
     return(
-      <p>Header</p>
+      <header className="top">
+        <h1>
+          Catch
+          <span className="ofThe">
+            <span className="of">of</span>
+            <span className="the">the</span>
+          </span>
+          Day
+        </h1>
+        <h3 className="tagline"><span>{this.props.tagline}</span></h3>
+      </header>
     )
   }
 }
