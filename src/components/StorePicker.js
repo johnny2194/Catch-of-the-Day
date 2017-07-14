49ec3d3d46b418f9ed74edac6658d5feea3f6aab
Johnny Watson <johnny@streetchange.org.uk>
2017-07-14 16:02:04 +0100

    Create StorePicker component and render it / mount it to main div in body

---
+++ src/components/StorePicker.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class StorePicker extends React.Component {     //Capital classname so can be used more than once
+
+  render() {
+    return <p>hello johnny</p>
+  }
+
+}   
+
+
+export default StorePicker;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-// let's go!
+import React from 'react'  //imports react library and assigns it to React variable.. listed in package.json
+import {render} from 'react-dom'
+import StorePicker from './components/StorePicker.js'  //needs ./ or will be searching in the node modules
+
+
+render(<StorePicker />, document.querySelector('#main'))  //mounts javascript to div in html body
+
