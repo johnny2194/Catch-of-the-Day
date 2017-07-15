d3b17a2579ed91837cca7ccf9d50b9a07cc3ae8e
Johnny Watson <johnny@streetchange.org.uk>
2017-07-15 11:04:38 +0100

    Create not found component and add routers for StorePicker as home, App for store/:storeId & miss to not found.  Also Add defaultValue to funny random name from helpers.js

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {getFunName} from '../helpers.js'
 
 class StorePicker extends React.Component {     //Capital classname so can be used more than once
 
   render() {
     return (
       <form className="store-selector">
-        <input type="text" placeholder="Store Name"/>
+        <input type="text" placeholder="Store Name" required  defaultValue={getFunName()}/>
         <button type="submit">Visit Store</button>
       </form> 
     )
--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react'  //imports react library and assigns it to React variable.. listed in package.json
 import {render} from 'react-dom'
+import {BrowserRouter, Match, Miss} from 'react-router'
+
 import './css/style.css'
 import App from './components/App'  //needs ./ or will be searching in the node modules
+import NotFound from './components/NotFound' 
 
+import StorePicker from './components/StorePicker' 
 
-render(<App/>, document.querySelector('#main'))  //mounts javascript to div in html body
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker}/>
+        <Match pattern="/store/:storeId" component={App}/>
+        <Miss component={NotFound}/>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+render(<Root/>, document.querySelector('#main'))  //mounts javascript to div in html body
 
