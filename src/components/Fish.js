4c6320843c30f24d37645b89b777f0dde34a48ab
Johnny Watson <johnny@streetchange.org.uk>
2017-07-16 15:22:38 +0100

    Import formatPrice function into Fish and envoke on price

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {formatPrice} from '../helpers.js'
 
 class Fish extends React.Component {
 
@@ -7,7 +8,7 @@ class Fish extends React.Component {
       <li className="menu-fish">
         <img src={this.props.image} alt={this.props.name}/>
         <h3 className="fish-name">{this.props.name}
-          <span className="price">{this.props.price}</span>
+          <span className="price">{formatPrice(this.props.price)}</span>
           </h3>
         <p>{this.props.description}</p>
         <button>Add to order</button>
