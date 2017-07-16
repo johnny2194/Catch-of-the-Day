fe9756f222a9fe14b65bc0c942a61e932501bc88
Johnny Watson <johnny@streetchange.org.uk>
2017-07-16 15:15:54 +0100

    Built out fish component with values passed in via props

--- src/components/Fish.js
+++ src/components/Fish.js
@@ -4,7 +4,14 @@ class Fish extends React.Component {
 
   render() {
     return(
-      <li className="menu-fish">🐟</li>
+      <li className="menu-fish">
+        <img src={this.props.image} alt={this.props.name}/>
+        <h3 className="fish-name">{this.props.name}
+          <span className="price">{this.props.price}</span>
+          </h3>
+        <p>{this.props.description}</p>
+        <button>Add to order</button>
+        </li>
       )
   }
 }
